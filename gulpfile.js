dac56a0af1ef302b42921502f639704db1b83412
Владислав <flatydoc@gmail.com>
2021-09-07 23:16:48 +0300

    add images

--- gulpfile.js
+++ gulpfile.js
@@ -93,19 +93,27 @@ function js() {
     .pipe(browsersync.stream());
 }
 
+function images() {
+  return src(path.src.img)
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream());
+}
+
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.css], css);
   gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
 }
 
 function clean(params) {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
